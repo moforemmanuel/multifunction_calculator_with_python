18af3cfbf3cec3c1740f87b08d5b328eb604db80
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2021-04-13 20:21:48 +0100

    Updating functions ...

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from math import *
+from sympy import *
 
 array = []
 
@@ -50,6 +51,9 @@ def multiply(array,n):
 		return array[0]
 	else:
 		return "Error\n"
+
+def rad(ans):
+	return (ans*180)/pi
 
 def divide(array,n):
 	
@@ -226,89 +230,474 @@ while True:
 				
 	
 	if GC == 2:
-			MC = str(input("""Choose a mode :
+		MC = str(input("""Choose a mode :
 		a) Degrees
 		b) Radian\n"""))
 					
-	while True:
-		BC = str(input("""Choose a Trigonometric Operation : 
-		i)   Trigonometric functions
-		ii)  Inverse trigonometric functions
-		iii) Exit\n"""))
+		while True:
+			BC = str(input("""Choose a Trigonometric Operation : 
+			i)   Trigonometric functions
+			ii)  Inverse trigonometric functions
+			iii) Exit\n"""))
+		
+			if BC == "i":
+				while True:
+					TF = str(input("""Choose a Trigonometric function : 
+					i)  sin
+					ii) cos
+					iii)tan
+					iv)cosec
+					v) sec
+					vi)cot
+					vii)exit\n"""))
+
+					if TF == "i":
+						input_angle = int(input("Input angle : "))
+						if MC == "a":
+							angle = radians(input_angle)
+						elif MC == "b":
+							angle = degrees(input_angle)
+						ans=sin(angle)
+						approx=round(ans,2)
+						print(f"sin({input_angle}) = {approx}")
+
+					if TF == "ii":
+						input_angle = int(input("Input angle : "))
+						if MC == "a":
+							angle = radians(input_angle)
+						elif MC == "b":
+							angle = degrees(input_angle)
+						ans=cos(angle)
+						approx=round(ans,2)
+						print(f"cos({input_angle}) = {approx}")
+
+					if TF == "iii":
+						input_angle = int(input("Input angle : "))
+						if MC == "a":
+							angle = radians(input_angle)
+						elif MC == "b":
+							angle = degrees(input_angle)
+						ans=tan(angle)
+						approx=round(ans,2)
+						print(f"tan({input_angle}) = {approx}")
+
+					if TF == "iv":
+						input_angle = int(input("Input angle : "))
+						if MC == "a":
+							angle = radians(input_angle)
+						elif MC == "b":
+							angle = degrees(input_angle)
+						ans=cosec(angle)
+						approx=round(ans,2)
+						print(f"cosec({input_angle}) = {approx}")
+
+					if TF == "v":
+						input_angle = int(input("Input angle : "))
+						if MC == "a":
+							angle = radians(input_angle)
+						elif MC == "b":
+							angle = degrees(input_angle)
+						ans=sec(angle)
+						approx=round(ans,2)
+						print(f"sec({input_angle}) = {approx}")
+
+					if TF == "vi":
+						input_angle = int(input("Input angle : "))
+						if MC == "a":
+							angle = radians(input_angle)
+						elif MC == "b":
+							angle = degrees(input_angle)
+						ans=cot(angle)
+						approx=round(ans,2)
+						print(f"cot({input_angle}) = {approx}")
+
+					if TF == "vii":
+						break
+				
+
+			if BC == "ii":
+				while True:
+					TF = str(input("""Choose a Trigonometric function : 
+					i)  arcsin
+					ii) arccos
+					iii)arctan
+					iv)arccosec
+					v) arcsec
+					vi)arccot
+					vii)exit\n"""))
+
+					if TF == "i":
+						input_number = float(input("Input number : "))
+						
+						ans=asin(input_number)
+						approx=round(ans,2)
+
+						if MC=="a":
+							final = rad(approx)
+
+							if input_number == 1 or isclose(90,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsin({input_number}) = 90")
+								break
+
+							if input_number == 0.866 or isclose(60,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsin({input_number}) = 60")
+								break
+
+							if input_number == 0.7071 or isclose(45,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsin({input_number}) = 45")
+								break
+
+							if input_number == 0.5 or isclose(30,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsin({input_number}) = 30")
+								break
+
+							if input_number == 0 or isclose(0,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsin({input_number}) = 0")
+								break
+
+							else:
+								print(f"arcsin({input_number}) = {final}")
+								break
+
+						elif MC == "b":
+							print(f"arcsin({input_number}) = {approx}")
+
+						else:
+							sys.exit(1)
+							
+
+					if TF == "ii":
+						input_number = float(input("Input number : "))
+						
+						ans=acos(input_number)
+						approx=round(ans,2)
+
+						if MC=="a":
+							final = rad(approx)
+
+							if input_number == 0 or isclose(90,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccos({input_number}) = 90")
+								break
+
+							if input_number == 0.5 or isclose(60,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccos({input_number}) = 60")
+								break
+
+							if input_number == 0.7071 or isclose(45,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccos({input_number}) = 45")
+								break
+
+							if input_number == 0.866 or isclose(30,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccos({input_number}) = 30")
+								break
+
+							if input_number == 1 or isclose(0,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccos({input_number}) = 0")
+								break
+
+							else:
+								print(f"arccos({input_number}) = {final}")
+								break
+
+						elif MC == "b":
+							print(f"arccos({input_number}) = {approx}")
+
+						else:
+							sys.exit(1)
+
+					if TF == "iii":
+						input_number = float(input("Input number : "))
+						
+						ans=atan(input_number)
+						approx=round(ans,2)
+
+						if MC=="a":
+							final = rad(approx)
+
+							if input_number == inf or isclose(90,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 90")
+								break
+
+							if input_number == 1.732 or isclose(60,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 60")
+								break
+
+							if input_number == 1 or isclose(45,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 45")
+								break
+
+							if input_number == 0.577 or isclose(30,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 30")
+								break
+
+							if input_number == 0 or isclose(0,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 0")
+								break
+
+							else:
+								print(f"arctan({input_number}) = {final}")
+								break
+
+						elif MC == "b":
+							print(f"arctan({input_number}) = {approx}")
+
+						else:
+							sys.exit(1)
+
+					if TF == "iv":
+						input_number = float(input("Input number : "))
+						
+						ans=acosec(input_number)
+						approx=round(ans,2)
+
+						if MC=="a":
+							final = rad(approx)
+
+							if input_number == 1 or isclose(90,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccosec({input_number}) = 90")
+								break
+
+							if input_number == 1.155 or isclose(60,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccosec({input_number}) = 60")
+								break
+
+							if input_number == 1.414 or isclose(45,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccosec({input_number}) = 45")
+								break
+
+							if input_number == 2 or isclose(30,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccosec({input_number}) = 30")
+								break
+
+							if input_number == inf or isclose(0,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arccosec({input_number}) = 0")
+								break
+
+							else:
+								print(f"arccosec({input_number}) = {final}")
+								break
+
+						elif MC == "b":
+							print(f"arccosec({input_number}) = {approx}")
+
+						else:
+							sys.exit(1)
+
+					if TF == "v":
+						input_number = float(input("Input number : "))
+						
+						ans=asec(input_number)
+						approx=round(ans,2)
+
+						if MC=="a":
+							final = rad(approx)
+
+							if input_number == inf or isclose(90,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsec({input_number}) = 90")
+								break
+
+							if input_number == 2 or isclose(60,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsec({input_number}) = 60")
+								break
+
+							if input_number == 1.414 or isclose(45,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsec({input_number}) = 45")
+								break
+
+							if input_number == 1.155 or isclose(30,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsec({input_number}) = 30")
+								break
+
+							if input_number == 1 or isclose(0,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arcsec({input_number}) = 0")
+								break
+
+							else:
+								print(f"arcsec({input_number}) = {final}")
+								break
+
+						elif MC == "b":
+							print(f"arcsec({input_number}) = {approx}")
+
+						else:
+							sys.exit(1)
+
+					if TF == "vi":
+						input_number = float(input("Input number : "))
+						
+						ans=atan(input_number)
+						approx=round(ans,2)
+
+						if MC=="a":
+							final = rad(approx)
+
+							if input_number == 0 or isclose(90,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 90")
+								break
+
+							if input_number == 0.577 or isclose(60,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 60")
+								break
+
+							if input_number == 1 or isclose(45,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 45")
+								break
+
+							if input_number == 1.732 or isclose(30,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 30")
+								break
+
+							if input_number == inf or isclose(0,rad(ans),rel_tol=1e-5,abs_tol=0.1)==True:
+								print(f"arctan({input_number}) = 0")
+								break
+
+							else:
+								print(f"arctan({input_number}) = {final}")
+								break
+
+						elif MC == "b":
+							print(f"arctan({input_number}) = {approx}")
+
+						else:
+							sys.exit(1)
+					if TF == "vii":
+						break
+					
+			if BC == "iii":
+				break
+
+	if GC == 3:
+			
+		while True:
+			HC = str(input("""Choose a Trigonometric Operation : 
+				i)   Hyperbolic functions
+				ii)  Inverse hyperbolic functions
+				iii) Exit\n"""))
 	
-		if BC == "i":
-			while True:
-				TF = str(input("""Choose a Trigonometric function : 
-				i)  sin
-				ii) cos
-				iii)tan
-				iv)cosec
-				v) sec
-				vi)cot
-				vii)exit\n"""))
+			if HC == "i":
+				while True:
+					HF = str(input("""Choose a Hyperbolic function : 
+					i)  sinh
+					ii) cosh
+					iii)tanh
+					iv)cosech
+					v) sech
+					vi)coth
+					vii)exit\n"""))
 
-				if TF == "i":
-					input_angle = int(input("Input angle : "))
-					if MC == "a":
-						angle = radians(input_angle)
-					elif MC == "b":
-						angle = degrees(input_angle)
-					ans=sin(angle)
-					approx=round(ans,2)
-					print(f"sin({input_angle} = {approx}")
+					input_number = float(input("Enter a number : \n"))
 
-				if TF == "ii":
-					input_angle = int(input("Input angle : "))
-					if MC == "a":
-						angle = radians(input_angle)
-					elif MC == "b":
-						angle = degrees(input_angle)
-					ans=cos(angle)
-					approx=round(ans,2)
-					print(f"cos({input_angle} = {approx}")
+					if  HF == "i":
+						ans=sinh(input_number)
+						approx=round(ans,2)
+						print(f"sinh({input_number}) = {approx} \n")
 
-				if TF == "iii":
-					input_angle = int(input("Input angle : "))
-					if MC == "a":
-						angle = radians(input_angle)
-					elif MC == "b":
-						angle = degrees(input_angle)
-					ans=tan(angle)
-					approx=round(ans,2)
-					print(f"tan({input_angle} = {approx}")
+					if  HF == "ii":
+						ans=cosh(input_number)
+						approx=round(ans,2)
+						print(f"cosh({input_number}) = {approx} \n")
 
-				if TF == "iv":
-					input_angle = int(input("Input angle : "))
-					if MC == "a":
-						angle = radians(input_angle)
-					elif MC == "b":
-						angle = degrees(input_angle)
-					ans=cosec(angle)
-					approx=round(ans,2)
-					print(f"cosec({input_angle} = {approx}")
+					if  HF == "iii":
+						ans=tanh(input_number)
+						approx=round(ans,2)
+						print(f"tanh({input_number}) = {approx} \n")
 
-				if TF == "v":
-					input_angle = int(input("Input angle : "))
-					if MC == "a":
-						angle = radians(input_angle)
-					elif MC == "b":
-						angle = degrees(input_angle)
-					ans=sec(angle)
-					approx=round(ans,2)
-					print(f"sec({input_angle} = {approx}")
+					if  HF == "iv":
+						ans=cosech(input_number)
+						approx=round(ans,2)
+						print(f"cosech({input_number}) = {approx} \n")
 
-				if TF == "vi":
-					input_angle = int(input("Input angle : "))
-					if MC == "a":
-						angle = radians(input_angle)
-					elif MC == "b":
-						angle = degrees(input_angle)
-					ans=cot(angle)
-					approx=round(ans,2)
-					print(f"cot({input_angle} = {approx}")
+					if  HF == "v":
+						ans=sech(input_number)
+						approx=round(ans,2)
+						print(f"sech({input_number}) = {approx} \n")
 
-				if TF == "vii":
-					break
+					if  HF == "vi":
+						ans=coth(input_number)
+						approx=round(ans,2)
+						print(f"coth({input_number}) = {approx} \n")
+
+					if HF == "vii":
+						break
+
+			if HC == "ii":
+				while True:
+					HF = str(input("""Choose an Inverse hyperbolic function : 
+					i)  arsinh
+					ii) arcosh
+					iii)artanh
+					iv)arcosech
+					v) arsech
+					vi)arcoth
+					vii)exit\n"""))
+
+					input_number = float(input("Enter a number : \n"))
+
+					if  HF == "i":
+						ans=asinh(input_number)
+						approx=round(ans,2)
+						print(f"arsinh({input_number}) = {approx} \n")
+
+					if  HF == "ii":
+						ans=acosh(input_number)
+						approx=round(ans,2)
+						print(f"arcosh({input_number}) = {approx} \n")
+
+					if  HF == "iii":
+						ans=atanh(input_number)
+						approx=round(ans,2)
+						print(f"artanh({input_number}) = {approx} \n")
+
+					if  HF == "iv":
+						ans=acosech(input_number)
+						approx=round(ans,2)
+						print(f"arcosech({input_number}) = {approx} \n")
+
+					if  HF == "v":
+						ans=asech(input_number)
+						approx=round(ans,2)
+						print(f"arsech({input_number}) = {approx} \n")
+
+					if  HF == "vi":
+						ans=acoth(input_number)
+						approx=round(ans,2)
+						print(f"arcoth({input_number}) = {approx} \n")
+
+					if HF == "vii":
+						break
+
+	if GC == "iv":
+
+		while True:
+			CC = str(input("""Choose a Conversion Operation : 
+				i)   Number systems
+				ii)  Temperature
+				iii) Angles
+				iv)  Exit\n"""))
+	
+			if CF == "i":
+				while True:
+					HF = str(input("""Choose a conversion function : 
+					i)  binary to decimal
+					ii) binary to octal
+					iii)binary to hexadecimal
+					iv)decimal to binary
+					v) decimal to octal
+					vi)decimal to hexadecimal
+					vii)decimal to other 
+					viii) octal to hexadecimal \n"""))
+					
+
+
+
+
+
+
+				
+
+					
+
+		
 		
                                                             
 					
